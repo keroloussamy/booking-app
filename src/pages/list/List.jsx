cb3d9ebd12441fc3946fdba2447d72244cc6e4f6
keroloussamy <keroloussamy98@gmail.com>
2022-05-28 16:40:39 +0200

    Add Header component and use react-date-rang

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+import './list.css'
 
 const List = () => {
   return (
-    <div>List</div>
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
   )
 }
 
