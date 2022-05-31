66394616c681012e07f082f9fc5e743b61b9e2db
keroloussamy <keroloussamy98@gmail.com>
2022-05-31 08:55:27 +0200

    Add PropertyList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
 
 const Home = () => {
@@ -11,6 +12,9 @@ const Home = () => {
       <Header />
       <div className='homeContainer'>
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
       </div>
     </div>
   )
