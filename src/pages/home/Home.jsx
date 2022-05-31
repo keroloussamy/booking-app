ea97533bbac96d94205dcccbdcefa65fc7c4508c
keroloussamy <keroloussamy98@gmail.com>
2022-05-31 08:26:27 +0200

    Add Featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
+import './home.css'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+        <Featured />
+      </div>
     </div>
   )
 }
