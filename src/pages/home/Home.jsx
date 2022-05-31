2fd190dc1d1046a7364b0f9847c5286bb83b06e5
keroloussamy <keroloussamy98@gmail.com>
2022-05-31 09:45:42 +0200

    Add Footer component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import MailList from '../../components/mailList/MailList'
+import Footer from '../../components/footer/Footer'
 import './home.css'
 
 const Home = () => {
@@ -19,6 +20,7 @@ const Home = () => {
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
     </div>
   )
