96bc335abec365977ecdafc675c5cddee98e7c6b
keroloussamy <keroloussamy98@gmail.com>
2022-06-12 12:51:37 +0200

    Add duplicate error handler

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import { CustomAPIError } from "../utils/custom-error.js"
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
@@ -11,7 +10,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     return res.status(422).json({ msg: err.message }) //422: unprocessable entity (used for validation errors)
   }
 
-  console.log(err.properties);
+  //duplicate key is 11000. catch errors with duplicate email, username, any something unique.
+  if ( err.code == 11000 ) {
+    let field = Object.keys(err.keyValue)[0];
+    let value = err.keyValue[field];
+    return res.status(409).json({ msg: `Duplicate ${field} with value ${value}` }) //"Already Exists" would be '409 Conflict'.
+  }
+
   const errorStatus = err.status || 500;
   const errorMessage = err.message || 'Something went wrong, please try again';
   return res.status(errorStatus).json({ msg: errorMessage })
