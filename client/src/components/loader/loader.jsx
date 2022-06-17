8db1bd71d6c34caa23fded99424c8e9b75a0fe3e
keroloussamy <keroloussamy98@gmail.com>
2022-06-17 03:02:11 +0200

    Update loader component

--- client/src/components/loader/loader.jsx
+++ client/src/components/loader/loader.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import "./loader.css";
+import loaderImg from '../../images/loader.gif'
 
-const Loader = ({ text = '', size = '5em' }) => {
-  const header = text ? <h4>{text}</h4> : null
+const Loader = ({ size = '5em' }) => {
   return (
-    <div className="spinner">
-      {header}
-      <div className="loader" style={{ height: size, width: size }} />
+    <div style={{ textAlign: 'center' }}>
+      <img src={loaderImg} alt='loader' style={{ height: size, width: size }} />
     </div>
   )
 }
