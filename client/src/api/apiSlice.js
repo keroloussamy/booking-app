292e44e768ff69e352d080b4d0689b314a80459c
keroloussamy <keroloussamy98@gmail.com>
2022-06-17 04:06:47 +0200

    Add getFeaturedProperties call

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -4,12 +4,16 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8800/api' }),
   endpoints: builder => ({
+    // Home page
     getCountByCity: builder.query({
       query: () => '/hotels/countByCity?cities=berlin,madrid,london'
     }),
     getCountByType: builder.query({
       query: () => '/hotels/countByType'
+    }),
+    getFeaturedProperties: builder.query({
+      query: () => '/hotels?featured=true&limit=4'
     })
   })
 })
-export const { useGetCountByCityQuery, useGetCountByTypeQuery } = apiSlice
+export const { useGetCountByCityQuery, useGetCountByTypeQuery, useGetFeaturedPropertiesQuery } = apiSlice
