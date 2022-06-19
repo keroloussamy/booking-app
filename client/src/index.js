2c941a5638622ed6b3f24271a2e7083402a0e3fb
keroloussamy <keroloussamy98@gmail.com>
2022-06-19 12:50:43 +0200

    Add SearchContextProvider to index file

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import App from './App';
+import { SearchContextProvider } from './context/SearchContext';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <SearchContextProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </SearchContextProvider>
   </React.StrictMode>,
 );
