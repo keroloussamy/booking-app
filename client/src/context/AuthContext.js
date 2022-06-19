8afae5810e67adf318e775764ddada53315acf03
keroloussamy <keroloussamy98@gmail.com>
2022-06-19 13:12:05 +0200

    Add AuthContextProvider

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,65 @@
+import { createContext, useReducer, useEffect } from "react";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null, //check if the user logged before or not first, so if he refresh the page we still have his information.
+  loading: false,
+  error: null,
+};
+
+const AuthContext = createContext(INITIAL_STATE);
+
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "LOGOUT":
+      //localStorage.removeItem("user");
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  //Save data in local storage, so if the user refresh the page, we still have his information.
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 import { SearchContextProvider } from './context/SearchContext';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 );
